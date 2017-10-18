c82803b178ec5b08c690e01a2f0e24c6a93f7edd
holalearn <32857765+holalearn@users.noreply.github.com>
2017-10-18 19:52:47 +0800

    Update CustomRepositoryConfig.java

--- src/main/java/example/springdata/jpa/customall/CustomRepositoryConfig.java
+++ src/main/java/example/springdata/jpa/customall/CustomRepositoryConfig.java
@@ -1,30 +1,11 @@
-/*
- * Copyright 2015-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package example.springdata.jpa.customall;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
- * Sample configuration to bootstrap Spring Data JPA through JavaConfig. Note how Spring Data JPA is configured with a
- * custom repository base class. This causes all repository interfaces being found for this configuration to use the
- * configured class as base repository.
- * 
- * @author Oliver Gierke
- * @soundtrack Tim Neuhaus - As life found you (The Cabinet)
+ * 通过JavaConfig的简单配置来启动Spring Data JPA. 注意如何为Spring Data JPA配置自定义repository基类
+ * 通过配置，所有的repository接口都会使用配置类作为repository基类.
  */
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = ExtendedJpaRepository.class)
