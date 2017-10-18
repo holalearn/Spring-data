01a1f7072001fb31a1be359c5393c4694c4fb1d1
holalearn <32857765+holalearn@users.noreply.github.com>
2017-10-18 14:15:31 +0800

    Update CustomerRepository.java

--- src/main/java/example/springdata/jpa/projections/CustomerRepository.java
+++ src/main/java/example/springdata/jpa/projections/CustomerRepository.java
@@ -15,84 +15,54 @@ import org.springframework.data.repository.CrudRepository;
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
 	/**
-	 * 使用投影接口指定返回字段. 因为投影不使用任何动态字段， doesn't use any dynamic
-	 * fields, the query execution will be restricted to only the fields needed by the projection.
-	 * 
-	 * @return
+	 * interface投影：接口方法指定返回字段.
 	 */
 	Collection<CustomerProjection> findAllProjectedBy();
 
 	/**
-	 * 如果投影包含动态属性(i.e.  {@link Value}注解内的SpEL表达式),会加载正常的target entity然后再动态投影，以方便表达式对于target的引用
-	 * 
-	 * @return
+	 * 动态属性投影：如：{@link Value}注解内的SpEL表达式,会加载正常的target entity然后再动态投影，以方便表达式对于target的引用
 	 */
 	Collection<CustomerSummary> findAllSummarizedBy();
 
 	/**
-	 * 投影接口也可以和手动声明的查询语句一起使用. 需要保证使用别名与投影字段匹配
-	 * 
-	 * @return
+	 * interface投影 & 手动声明查询 ： 需要保证使用别名与投影字段匹配
 	 */
 	@Query("select c.firstname as firstname, c.lastname as lastname from Customer c")
 	Collection<CustomerProjection> findsByProjectedColumns();
 
 	/**
-	 * 使用具体的DTO类型来指定返回字段. This gets translated into a constructor expression
-	 * in the query.
-	 * 
-	 * @return
+	 * DTO投影：使用具体的DTO类型来指定返回字段. 这个将转换为查询语句中的构造函数表达式
 	 */
 	Collection<CustomerDto> findAllDtoedBy();
-
+	
 	/**
-	 * 动态传入投影类型(interface或DTO).
-	 * 
-	 * @param firstname
-	 * @param projection
-	 * @return
-	 */
-	<T> Collection<T> findByFirstname(String firstname, Class<T> projection);
-
-	/**
-	 * 单个记录的投影.
-	 * 
-	 * @param id
-	 * @return
-	 */
-	CustomerProjection findProjectedById(Long id);
-
-	/**
-	 * 动态投影单个记录.
-	 * 
-	 * @param id
-	 * @param projection
-	 * @return
-	 */
-	<T> T findProjectedById(Long id, Class<T> projection);
-
-	/**
-	 * 翻页 & 投影
-	 * 
-	 * @param pageable
-	 * @return
-	 */
-	Page<CustomerProjection> findPagedProjectedBy(Pageable pageable);
-
-	/**
-	 * A DTO projection using a constructor expression in a manually declared query.
-	 * 
-	 * @param firstname
-	 * @return
+	 * DTO投影：在声明的查询语句中使用构造器表达式
 	 */
 	@Query("select new example.springdata.jpa.projections.CustomerDto(c.firstname) from Customer c where c.firstname = ?1")
 	Collection<CustomerDto> findDtoWithConstructorExpression(String firstname);
 
 	/**
+	 * 动态类型投影：(interface或DTO).
+	 */
+	<T> Collection<T> findByFirstname(String firstname, Class<T> projection);
+
+	/**
+	 * 单个记录的投影.
+	 */
+	CustomerProjection findProjectedById(Long id);
+
+	/**
+	 * 动态投影单个记录.
+	 */
+	<T> T findProjectedById(Long id, Class<T> projection);
+
+	/**
+	 * 翻页 & 投影
+	 */
+	Page<CustomerProjection> findPagedProjectedBy(Pageable pageable);
+
+	/**
 	 * Optional & 投影
-	 * 
-	 * @param lastname
-	 * @return
 	 */
 	Optional<CustomerProjection> findOptionalProjectionByLastname(String lastname);
 }
